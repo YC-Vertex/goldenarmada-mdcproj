37fbd6fa72cc3e5632cbb5818f2bbb03f098301d
JhZhang-1999 <jiahui-z18@mails.tsinghua.edu.cn>
2019-11-17 14:38:58 +0800

    11/17 zjh update init_cata & init_color

--- feedback/init_pkg/init_cata.py
+++ feedback/init_pkg/init_cata.py
@@ -1,5 +1,86 @@
-def cata(img_grey):
-    # 一通操作，输入灰度图像，返回一个值，为类别标签
-    cata_tag=-1
+import numpy as np
+import tensorflow as tf
+import keras
+import cv2
 
-    return cata_tag
+### quotes:
+# https://blog.csdn.net/geyalu/article/details/50190121
+
+def inverse(image): 
+    '''
+    :param image: 以数组形式存储的图片数据（单通道2维，即长乘宽）
+    :return: 反色后的数组形式存储的图片数据
+
+    I use this function to inverse the color of a image, because:
+    a. cv2.normalize will make the background all white and content all black, but
+    b. the CNN requires the picture to have black background and white content.
+    '''
+    height,width = image.shape
+    inversed = image.copy()
+    for i in range(height):
+        for j in range(width):
+            inversed[i,j] = (255-image[i,j]) 
+    return inversed
+
+def read_my_data(my_img_path):
+    '''
+    :param my_img_path: a string, the location of the img
+    :return: numpy array of size (1,28,28,1), to be fed into CNN
+    '''
+    img = cv2.imread(my_img_path,0) # 灰度方式读取图片原始信息，2维，原始长乘宽
+    img = cv2.Canny(img,20,65) # 将图片处理成边缘形式，shape不变（这步≈正则化+反色，但是效果不太一样）
+    img = cv2.normalize(img,dst=None,alpha=300,beta=10,norm_type=cv2.NORM_MINMAX) # 正则化提升对比度，shape不变
+    img = cv2.resize(img, (28,28)) # 大小resize为28*28
+    # img = inverse(img) # 反色操作，shape不变
+    # cv2.imshow('图',img) # 展示图片
+    # cv2.waitKey(0) # 展示图片，暂停
+    my_test_x = img.astype('float32')/255 # 模型要求的归一化
+    my_test_x = my_test_x[np.newaxis,:,:,np.newaxis] # 模型要求的维度扩张，最终维度为(1,28,28,1)
+    return my_test_x
+
+def newargmax(prediction):
+    '''
+    :param prediction: 对衣服类别的预测结果，对每个category（共10个，包括鞋包等无用category）提供一个数值置信度
+    :return: 类别标签，0短袖、1裤子、2长袖
+    '''
+    valid_index = [0,1,2,4,6]
+    maxpi = -100000
+    res = -1
+    for i in valid_index:
+        if prediction[i] >= maxpi:
+            maxpi = prediction[i]
+            res = i
+    if res==4 or res==6:
+        res = 2
+    return res
+
+def cata(img_path):
+    """
+    :lib: feedback
+    :func: cata
+    :param: img_path
+    :return: res (category tag)
+
+    Return the category tag of a image, given the image path. 
+    """
+    try:
+        # 加载本地图片
+        my_test_x = read_my_data(img_path)
+    except:
+        print("data error: failed to read data.")
+    else:
+        try:
+            # 加载本地模型
+            loaded_model = keras.models.load_model("path/model.h5")
+        except:
+            print("model error: failed to load CNN model.")
+        else:
+            # 使用模型进行预测
+            try:
+                y_pred = loaded_model.predict(my_test_x, batch_size=1) # 预测我的数据
+            except:
+                print("model error: failed to make prediction.")
+            else:
+                # 输出预测结果
+                res = newargmax(y_pred[0])
+    return res
--- feedback/init_pkg/init_color.py
+++ feedback/init_pkg/init_color.py
@@ -1,5 +1,130 @@
-def color(img):
-    # 一通操作，输入图像，返回一个值，为颜色标签
-    color_tag=-1
+import colorsys
+from PIL import Image
+import optparse
+import webcolors
 
-    return color_tag
+### quotes:
+# https://www.jb51.net/article/48875.htm
+# https://stackoverflow.com/questions/9694165/convert-rgb-color-to-english-color-name-like-green-with-python
+# https://www.hangge.com/blog/cache/detail_981.html
+
+def color_to_color(name):
+    '''
+    :param name: 字符串，css3标准中的一种颜色名称，全小写、无空格
+    :return: 颜色标签，0~9（红0，粉1，黄橙2，紫3，绿4，蓝5，棕6，白7，灰8，黑9），出错则返回-1
+    '''
+    redlist = ['indianred','lightcoral','salmon','darksalmon','lightsalmon','crimson','red','firebrick','darkred']
+    pinklist = ['pink','lightpink','hotpink','deeppink','mediumvioletred','palevioletered']
+    yellowlist = ['coral','tomato','orangered','darkorange','orange',\
+        'gold','yellow','lightyellow','lemonchiffon','lightgodenrodyellow','papayawhip','moccasin','peachpuff','palegoldenrod','khaki','darkkhaki']
+    purplelist = ['lavender','thistle','plum','violet','orchid','fuchsia','magenta','mediumorchid','mediumpurple','amethyst','blueviolet','darkviolet','darkorchid','darkmagenta','purple','indigo','slateblue','dakrslateblue','mediumslateblue']
+    greenlist = ['greenyellow','chartreuse','lawngreen','lime','limegreen','palegreen','lightgreen','mediumspringgreen','springgreen','mediumseagreen','seagreen','forestgreen','green','darkgreen','yellowgreen','olivedrab','olive','darkolivegreen','mediumaquamarine','darkseagreen','lightseagreen','darkcyan','teal']
+    bluelist = ['aqua','cyan','lightcyan','paleturquoise','aquamarine','turquoise','mediumturquoise','darkturquoise','cabetblue','steelblue','lightsteelblue','powderblue','lightblue','skyblue','lightskyblue','deepskyblue','dodgerblue','cornflowerblue','mediumslateblue','royalblue','blue','mediumblue','darkblue','navy','midnightblue']
+    brownlist = ['cornsilk','blanchedalmond','bisque','navajowhite','wheat','burlywood','tan','rosybrown','sandybrown','goldenrod','darkgoldenrod','peru','chocolate','saddlebrown','sienna','brown','maroon']
+    whitelist = ['white','lightgray','snow','honeydew','mintcream','azure','aliceblue','ghostwhite','whitesmoke','seashell','beige','oldlace','floralwhite','ivory','antiquewhite','linen','lavenderblush','mistyrose']
+    graylist = ['gainsboro','silver','darkgray','gray','dimgray','lightslategray','slategray','darkslategray']
+    blacklist = ['black']
+    if name in redlist:
+        return 0
+    if name in pinklist:
+        return 1
+    if name in yellowlist:
+        return 2
+    if name in purplelist:
+        return 3
+    if name in greenlist:
+        return 4
+    if name in bluelist:
+        return 5
+    if name in brownlist:
+        return 6
+    if name in whitelist:
+        return 7
+    if name in graylist:
+        return 8
+    if name in blacklist:
+        return 9
+    else:
+        return -1
+
+def closest_colour(requested_colour):
+    '''
+    :param requested_colour: 图片的一个最主色，为rgb三色值的tuple
+    :return: 字符串，css3标准中的一个颜色名
+
+    本函数辅助"get_colour_name"函数，不单独使用。
+    '''
+    min_colours = {}
+    for key, name in webcolors.css3_hex_to_names.items():
+        r_c, g_c, b_c = webcolors.hex_to_rgb(key)
+        rd = (r_c - requested_colour[0]) ** 2
+        gd = (g_c - requested_colour[1]) ** 2
+        bd = (b_c - requested_colour[2]) ** 2
+        min_colours[(rd + gd + bd)] = name
+    return min_colours[min(min_colours.keys())]
+
+def get_colour_name(requested_colour):
+    '''
+    :param requested_color: 图片的一个最主色，为rgb三色值的tuple
+    :return: 字符串，css3标准中的一个颜色名
+    '''
+    try:
+        actual_name = webcolors.rgb_to_name(requested_colour)
+        return actual_name
+    except:
+        closest_name = closest_colour(requested_colour)
+        return closest_name
+
+def get_dominant_color(image):
+    '''
+    :param image: 通过Image.open方式获得的图片文件
+    :return: 图片的主色，rgb三色值的tuple
+    '''
+    image = image.convert('RGBA')
+    image.thumbnail((200, 200))
+    max_score = 1
+    dominant_color = None
+    for count, (r, g, b, a) in image.getcolors(image.size[0] * image.size[1]):
+        if a == 0:
+            continue
+        saturation = colorsys.rgb_to_hsv(r / 255.0, g / 255.0, b / 255.0)[1]
+        y = min(abs(r * 2104 + g * 4130 + b * 802 + 4096 + 131072) >> 13, 235)
+        y = (y - 16.0) / (235 - 16)
+        if y > 0.9:
+            continue
+        score = (saturation + 0.1) * count
+        if score > max_score:
+            max_score = score
+            dominant_color = (r, g, b)
+    return dominant_color
+
+def color(img_path):
+    """
+    :lib: feedback
+    :func: color
+    :param: img_path
+    :return: color_name (color tag)
+
+    Return the color tag of a image, given the image path. 
+    """
+    try:
+        # 通过PIL库的方式加载本地图片
+        img = Image.open(img_path)
+    except:
+        print("data error: failed to open image using Image from PIL")
+    else:
+        try:
+            # 获取图片主色
+            color_value = get_dominant_color(img)
+        except:
+            print("func error: func'get_dominant_color' has an error")
+        else:
+            try:
+                # 获取图片主色的详细名称
+                d_name = get_colour_name(color_value)
+            except:
+                print("func error: func'get_colour_name' has an error")
+            else:
+                # 转换为正式的颜色标签
+                color_name = color_to_color(d_name)
+                return color_name
