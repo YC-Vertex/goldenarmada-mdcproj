854aa958d7e6f5ef65f3c9f117bd50429729fb52
JhZhang-1999 <jiahui-z18@mails.tsinghua.edu.cn>
2019-11-17 15:40:46 +0800

    11/17 zjh modify clocam.py

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 Private repo of team Golden Armada in THU Mechanics Design Competition
 
+## 20191117 15:36 - zjh：
+
+更新clocam.py，（试图）完成了这部分对DB的调用。如果成功的话应该能做到把路径+标签存进数据库
+
+现在三个问题：
+1. preference的更新感觉可以直接AI识别完情感后调一下DB更新偏好值就完事了，没必要再经过我这边，不然设计三个模块也不好搞
+2. controller还是空的，所以没法运行和debug，我现在懵逼不知道写的东西能不能跑起来
+3. clocam里的shot_and_get函数是这么运作的：外面有个东西过来调它，给它传个参数ctr记录这是第几次让它拍衣服的照片，然后它就拍个照产生一个标签，存进数据库里。所以现在需要有人来调它。
+
 ## 20191117 14:40 - zjh：
 
 完善init cata（识别衣服类别，分成短袖、裤子、长袖）和init color（识别衣服主色，分成0~9十种颜色），这两个功能实现完了
--- feedback/clocam.py
+++ feedback/clocam.py
@@ -1,8 +1,7 @@
 # 上乘feedback，下接三个init函数
-
 import sys
 sys.path.append('../') # 不然没法import上层目录里的feedback.py
-from myFeedback import feedback
+from goldenarmada-mdcproj import feedback
 import json
 import cv2
 import numpy as np
@@ -12,46 +11,36 @@ from init_pkg import init_color
 from picamera import PiCamera
 from time import sleep
 
-def shot_and_get():
+def shot_and_get(json):
     """
     :lib: feedback
     :func: shot_and_get
-    :param: 
-    :return: tag (1+1+1)
-
-    Capture a img of clothes when called, and return the tag of it.
+    :param: ctr: the time this func has been called
+    :return: none
     """
+    ctr = jason['param']
+    # 设置图片存储路径
+    path_suffix = '.jpg'
+    path_prefix = '/path/my_img'
+    img_path = path_prefix + str(ctr) + path_suffix
+    # 拍摄图片
+    camera = PiCamera()
+    camera.start_preview()
+    sleep(2)
+    camera.capture(img_path)
+    camera.stop_preview()
+    # 存储图片和标签
+    pattern_tag = init_pattern.pattern(img_path)
+    cata_tag = init_cata.cata(img_path)
+    color_tag = init_color.color(img_path)
     request = [{
-        'lib': 'intel',
-        'func': 'to_shot_clothes',
-        'param': ''
+        'lib': 'DB',
+        'func': 'InputClothes',
+        'param': {'category':cata_tag,\
+            'color':color_tag,\
+            'pattern':pattern_tag,\
+            'path':img_path}
     }]
     json_request = json.dumps(request)
-    shot_order = feedback.queryOutHandler(json_request)
-    if shot_order==True:
-        camera=PiCamera()
-        camera.start_preview()
-        sleep(5)
-        camera.capture('path/img.jpg')
-        camera.stop_preview()
-        img=cv2.imread('path/picture.jpg',1)
-        tag=get_tag(img)
-        return tag
-    else:
-        return [-1,-1,-1]
-
-def get_tag(img):
-    """
-    :lib: feedback
-    :func: get_tag
-    :param: img
-    :return: tag (1+1+1)
-
-    Get the tag of the clothes and return. 
-    """
-    img_grey=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    pattern_tag=init_pattern.pattern(img)
-    cata_tag=init_cata.cata(img_grey)
-    color_tag=init_color.color(img)
-    tag=[pattern_tag]+[cata_tag]+[color_tag]
-    return tag
+    retval = feedback.queryOutHandler(json_request)
+    return retval
